b42638960b24b6b87f96b02f16f3c8d2c6064da4
zynn <166290145+Code-by-zynn205@users.noreply.github.com>
2024-04-29 14:35:55 +0700

    Add files via upload

---
+++ ConectDatabase/Do_an_co_so/src/main/java/dacs/hibernate_demo2/App.java
@@ -0,0 +1,13 @@
+package dacs.hibernate_demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
