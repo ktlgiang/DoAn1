6dca97fec222e57da3a2786caa174a0159fca370
Giang <giangpro1935@gmail.com>
2024-05-09 22:35:15 +0700

    add Userapplication

--- src/main/java/org/example/doan/UserApplication.java
+++ src/main/java/org/example/doan/UserApplication.java
@@ -1,2 +1,20 @@
-package org.example.doan;public class UserApplication {
+package org.example.doan;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UserApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1005, 650);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
