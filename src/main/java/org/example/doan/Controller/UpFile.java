f3050a5b718f8e41ee774fbe7c8e127a1fcc228e
Giang <giangpro1935@gmail.com>
2024-05-11 23:00:22 +0700

    updtae

--- src/main/java/org/example/doan/Controller/UpFile.java
+++ src/main/java/org/example/doan/Controller/UpFile.java
@@ -8,11 +8,11 @@ import org.example.doan.HelloApplication;
 
 import java.io.IOException;
 
-public class UpFile1 extends Application {
+public class UpFile extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("upfile.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 250);
+        Scene scene = new Scene(fxmlLoader.load(), 680, 450);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
